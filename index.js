ed69c648c4ffbf3b41a7fa0c2919a163f3b15e7c
Adolf <adolfnyerere@gmail.com>
2023-09-29 01:02:19 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+
+function writeCards(myNewArr, event ){
+    const myArr = [];
+
+    for (let i = 0; i < myNewArr.length; i++) { 
+
+        let newArr = `Thank you, ${myNewArr[i]}, for the wonderful ${event} gift!`;
+              
+        myArr.push(newArr);
+    };
+    return (myArr);
+};
+
+ function countDown(number){
+    while (number >= 0){
+        console.log(number);
+        number--;
+    }
+ }
